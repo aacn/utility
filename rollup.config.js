59550ed08f43ba4d2c95a2d6dd0818768fecdd07
Marlon Kerth <MarlonAACN@users.noreply.github.com>
2024-11-18 09:40:04 +0100

    Wrap rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,4 @@
-import {
-  main as mainPath,
-  module as modulePath,
-  peerDependencies,
-} from './package.json';
+import pkg from './package.json' with { type: 'json'};
 
 import { swc } from 'rollup-plugin-swc3';
 import del from 'rollup-plugin-delete';
@@ -11,47 +7,48 @@ import commonjs from '@rollup/plugin-commonjs';
 import flatDts from 'rollup-plugin-flat-dts';
 import prettier from 'rollup-plugin-prettier';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import typescriptPaths from "rollup-plugin-typescript-paths";
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
+import path from 'node:path';
 
 const config = [
   {
     input: 'src/index.ts',
-    external: Object.keys(peerDependencies || {}),
+    external: Object.keys(pkg.peerDependencies || {}),
     plugins: [
-        del({ targets: ['dist/*', 'playground/src/component-lib/*'] }),
-        nodeResolve(),
-        typescriptPaths({
-          preserveExtensions: true,
-        }),
-        json(),
-        commonjs(),
-        nodePolyfills(), // Circular dependencies warning cant be fixed and will be ignored until further notice
-        // Transpile with swc
-        swc({
-          jsc: {
-            baseUrl: "./src",
-            paths: { "@/*": ["*"] },
-            parser: {
-              syntax: "typescript",
-              tsx: true
-            },
-            target: "es5",
-            loose: false,
-            minify: {
-              compress: false,
-              mangle: false
-            }
+      del({ targets: ['dist/*', 'playground/src/component-lib/*'] }),
+      nodeResolve(),
+      typescriptPaths({
+        preserveExtensions: true,
+      }),
+      json(),
+      commonjs(),
+      nodePolyfills(), // Circular dependencies warning cant be fixed and will be ignored until further notice
+      // Transpile with swc
+      swc({
+        jsc: {
+          baseUrl: path.resolve('./src'),
+          paths: { '@/*': ['*'] },
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
           },
-        }),
-        prettier(),
+          target: 'es5',
+          loose: false,
+          minify: {
+            compress: false,
+            mangle: false,
+          },
+        },
+      }),
+      prettier(),
     ],
-      output: [
-          // Outputs the packaged lib in CommonJS format
-          { file: mainPath, format: 'cjs', plugins: [flatDts()] },
-          // Outputs the packaged lib in ES Module format
-          { file: modulePath, format: 'esm', plugins: [flatDts()] },
-      ]
+    output: [
+      // Outputs the packaged lib in CommonJS format
+      { file: pkg.main, format: 'cjs', plugins: [flatDts()] },
+      // Outputs the packaged lib in ES Module format
+      { file: pkg.module, format: 'esm', plugins: [flatDts()] },
+    ],
   },
 ];
 
